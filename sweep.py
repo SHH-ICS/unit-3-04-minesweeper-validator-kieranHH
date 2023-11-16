6a8b2d47c976d48b8e733b9a7650aa3e997ba6a8
kieranHH <144241789+kieranHH@users.noreply.github.com>
2023-11-15 19:03:17 -0500

    Update sweep.py
    ???

--- sweep.py
+++ sweep.py
@@ -15,3 +15,27 @@ grid = [
   [0,0,0]
 ]
 print (validate(grid))
+
+mine_count = sum(row.count(-1) for row in grid)
+if grid[1][1] != -1 or mine_count != 1:
+        return False
+for i in range(3):
+      for j in range(3):
+        if grid[i][j] != -1:
+           count = 0
+          for x in range(max(0, i - 1), min(3, i + 2)):
+             for y in range(max(0, j - 1), min(3, j + 2)):
+               if grid[x][y] == -1:
+                count += 1
+                if int(grid[i][j]) != count:
+return False
+return True
+
+block = [
+    [1, -1, 1],
+    [1, 1, 1],
+    [1, 1, 1]
+]
+
+result = validate_minesweeper_block(block)
+print(result)
